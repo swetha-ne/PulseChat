884004209d4828e3c019281b57cac31db804e776
Swetha Nethmi <snethmi18@gmail.com>
2025-10-17 22:01:18 +0530

    built chatform component

--- pulsechat/app/components/ChatForm.tsx
+++ pulsechat/app/components/ChatForm.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+"use client";
 
-const ChatForm = () => {
+import React, { useState } from "react";
+
+const ChatForm = ({
+  onSendMessage,
+}: {
+  onSendMessage: (message: string) => void;
+}) => {
+  const [message, setMessage] = useState("");
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (message.trim() === "") {
+      setMessage("");
+    }
+  };
+
   return (
-    <div>
-      Chat Form
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit} className="flex gap-2 mt-4">
+      <input
+        type="text"
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Type your message..."
+        className=" flex-1 px-4 border-2 py-2 rounded-lg focus:outline-none"
+      />
+      <button type="submit" className=" text-white px-4 rounded-lg bg-blue-500">
+        Send
+      </button>
+    </form>
+  );
+};
 
-export default ChatForm
+export default ChatForm;
--- pulsechat/app/page.tsx
+++ pulsechat/app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
 import ChatForm from "./components/ChatForm";
 
 
 export default function Home() {
+  const handleSendMessage = (message: string) => {
+    console.log(message);
+  }
   return (
     <div className="flex mt-24 justify-center w-full">
       <div className="w-full max-w-3xl mx-auto">
@@ -9,7 +13,7 @@ export default function Home() {
         <div>
           TODO: Add Chat Room
         </div>
-        <ChatForm/>
+        <ChatForm onSendMessage={handleSendMessage}/>
       </div>
     </div>
   );
