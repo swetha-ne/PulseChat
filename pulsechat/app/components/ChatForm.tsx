588091e89a755552ca6d337b3d211b91152e0f2d
Swetha Nethmi <snethmi18@gmail.com>
2025-10-17 21:44:36 +0530

    create Chatform compoents

---
+++ pulsechat/app/components/ChatForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatForm = () => {
+  return (
+    <div>
+      Chat Form
+    </div>
+  )
+}
+
+export default ChatForm
--- pulsechat/app/layout.tsx
+++ pulsechat/app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="mx-auto max-w-7xl px-4">
+          {children}
+        </div>
+        
       </body>
     </html>
   );
